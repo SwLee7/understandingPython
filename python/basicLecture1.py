c943c53141a8cf7d468e7c9a214c0109cf63fa12
SeokWonLee <wondersw7@gmail.com>
2023-06-16 19:31:26 +0900

    List Dictionary 간단 정리

---
+++ python/basicLecture1.py
@@ -0,0 +1,11 @@
+# List
+중고차 = ['K5', 'white', 5000]
+# List 수정하기
+중고차[1] = 'black'
+# sort(): 정렬메서드...
+
+# Dictionary
+중고차2 = {'brand': 'BMW', 'Model': '520d', 'price': 5000}
+# Dictionary 수정하기
+중고차2['brand'] = 'Mercedes'
+print(중고차2['brand'])
