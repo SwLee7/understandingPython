5be22aff75abfa0f2d9134c0dee511e547a7d117
SeokWonLee <wondersw7@gmail.com>
2023-06-16 19:49:40 +0900

    for문 간단 정리

---
+++ python/for.py
@@ -0,0 +1,10 @@
+# for 반복문 : 여러번 반복해서 작성하고 싶을 때 사용 및 List에서 자료를 하나씩 봅을 때=> js의 for 문의 역할을한다.
+
+for i in range(0,10) :
+    print ('BMW 있어요.')
+    # 이렇게 작성하면 10번 반복
+
+중고차들 = ['K5', 'BMW', 'Mercedes']
+
+for i in 중고차들 :
+    print(i)
