8b7df5c1de45d542ef5dfe5d3e7e10c16415906a
SwLee7 <wondersw7@gmail.com>
2023-07-14 19:51:06 +0900

    programmers test

---
+++ python/strinstr.py
@@ -0,0 +1,13 @@
+# 문자열 str1, str2가 매개변수로 주어집니다.
+# str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+# 풀이
+def solution(str1, str2):
+    if str2 in str1:
+        answer = 1
+    else: answer = 0
+    return answer
+
+# 삼항 연산자를 이용한 풀이
+def solution(str1, str2):
+    return 1 if str2 in str1 else 2
