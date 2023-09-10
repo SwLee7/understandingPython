798f09edbfaaa468cf77ca7c6e0a40d7d5bef1fc
SeokWonLee <wondersw7@gmail.com>
2023-09-10 20:33:46 +0900

    programmers 이상한문자만들기 추가 복습

--- python/이상한문자만들기.py
+++ python/이상한문자만들기.py
@@ -17,11 +17,20 @@ def solution(s):
     s_list = s.split(' ')
     # 빈 문자열을 기준으로 잘라내는 리스트를 만든다.
     for i in s_list:
+        # i가 리스트를 순회하면서
         for j in range(len(i)):
+            # i의 길이만큼 j가 순회하는데
             if j % 2 == 0:
+                # 만약 j가 짝수라면
                 answer += i[j].upper()
+                # answer에 i[j]의 값을 대문자로 바꿔 더해간다.
             else :
+                # 만약 j가 홀수라면
                 answer += i[j].lower()
+                # answer에 i[j]의 값을 소문자로 바꿔 더해간다.
         answer += ' '
+        # answer에 띄어쓰기를한다.
     return answer[0:-1]
+    # 이렇게 생긴 answer의 0번 째 인덱스부터 마지막 인덱스의 전까지의 값만 리턴한다.
+    # 왜냐하면 answer 전체를 리턴하면 마지막 띄어쓰기까지 리턴하기 때문에 오답이 나온다. ** 주의! 
 print (solution('hello world'))
