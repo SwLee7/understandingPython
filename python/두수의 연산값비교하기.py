9e97948a424858de76ad4760e55dd3f967ce7901
SwLee7 <wondersw7@gmail.com>
2023-11-16 18:14:40 +0900

    programmers 두 수의 연산값 비교하기.py

---
+++ python/두수의 연산값비교하기.py
@@ -0,0 +1,18 @@
+# 문제 설명
+# 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+# 12 ⊕ 3 = 123
+# 3 ⊕ 12 = 312
+# 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+
+# 단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+
+# solution
+def solution(a, b):
+    answer = 0
+    operator_x = int(str(a)+str(b))
+    if operator_x < 2*a*b:
+        answer = 2*a*b
+    else:
+        answer = operator_x
+    return answer
