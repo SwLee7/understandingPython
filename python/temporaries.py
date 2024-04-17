427fb6dafa35f2cb22b6ad61c2049d6b2963c799
SeokWonLee <wondersw7@gmail.com>
2024-04-17 23:10:43 +0900

    programmers 삼각형의 조건.py

---
+++ python/temporaries.py
@@ -0,0 +1,9 @@
+jdj = ["tu", "me", "manques", "encore"]
+
+def say_french(jdj):
+    cequejedis = ''
+    for i in range(len(jdj)):
+        cequejedis += jdj[i]
+        return cequejedis
+    
+    print(say_french)
